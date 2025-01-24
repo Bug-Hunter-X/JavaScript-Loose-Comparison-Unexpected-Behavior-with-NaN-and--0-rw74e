c9ad87f71dbae9f69775160189dcb4f560e4cf2a
BugHunter-X <askzenai@gmail.com>
2025-01-24 16:02:06 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,11 @@
+function foo(a, b) {
+  if (a === b) {
+    return true; 
+  } else {
+    return false; 
+  }
+}
+
+console.log(foo(NaN, NaN)); //false
+console.log(foo(0, -0)); //false
+console.log(foo(undefined, undefined)); //false
